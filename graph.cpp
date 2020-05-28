517126158c4eda8a0c092b06f9e1af5cb9b3b93a
Abhijeet Kumar <44672451+Abhijeet05061998@users.noreply.github.com>
2020-05-28 15:24:24 +0530

    graph2
    DFS & BFS implementation

--- graph.cpp
+++ graph.cpp
@@ -1,8 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-
-void print(int**edge,int v,int sv,bool* visited)
+void printDFS(int**edge,int v,int sv,bool* visited)
 {
     cout<<sv<<endl;
     visited[sv]=true;
@@ -18,17 +16,47 @@ void print(int**edge,int v,int sv,bool* visited)
         {
             continue;
         }
-        print(edge,v,i,visited);
+        printDFS(edge,v,i,visited);
     }
 
   }
+}
+
+void printBFS(int**edge,int n,int sv)
+{
+    queue<int>pendingvertices;
+    bool*visited=new bool[n];
+    for(int i=0;i<n;i++)
+    {
+        visited[i]=false;
+    }
+    pendingvertices.push(sv);
+    visited[sv]=true;
+    while(pendingvertices.size()!=0)
+    {
+        int currentvertex=pendingvertices.front();
+        pendingvertices.pop();
+        cout<<currentvertex<<endl;
+        for(int i=0;i<n;i++)
+        {
+            if(i==currentvertex)
+            {
+                continue;
+            }
+            if(edge[currentvertex][i]==1&&  !visited[i])
+            {
+                pendingvertices.push(i);
+                visited[i]=true;
+            }
+        }
+    }
 }
 int main()
 {
     int v,e;
     cin>>v>>e;
     int**edge=new int*[v];
-    for(int i=0;i<v;i++)
+     for(int i=0;i<v;i++)
     {
         edge[i]=new int[v];
         for(int j=0;j<v;j++)
@@ -48,5 +76,9 @@ int main()
     {
         visited[i]=false;
     }
-    print(edge,v,0,visited);
+    cout<<"DFS:"<<endl;
+    printDFS(edge,v,0,visited);
+
+    cout<<"BFS:"<<endl;
+    printBFS(edge,v,0);
 }
